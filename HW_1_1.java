c5923501357150ab3958782e492c942c93a17926
dotcom07 <dotcombubblecom@gmail.com>
2024-11-07 12:11:56 +0900

    Initial commit

---
+++ HW_1_1.java
@@ -0,0 +1,12 @@
+public class HW_1_1 {
+
+  	public static void main(String[] args) {
+        System.out.println("50 Yonsei-ro Seodaemun-gu");
+        System.out.println("Seoul 03722");
+        System.out.println("Republic of Korea");
+        System.out.println("Gil-Dong Hong");
+        System.out.println("018-3924-2345");
+        System.out.println("gdh@yonsei.ac.kr");
+    }
+  
+}
---
+++ HW_1_2.java
@@ -0,0 +1,11 @@
+public class HW_1_2 {
+
+    public static void main(String[] args) {
+        
+        double radius = 3.5;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("area = " + area);
+          
+    }
+  
+}
